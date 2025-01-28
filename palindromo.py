d97c39e12992da9db9c02ae521e51e6cfe741630
Bianca Abbamonte Aveno <bianca.abbamonte@yahoo.com.br>
2025-01-27 23:14:34 -0300

    Update palindromo.py

--- palindromo.py
+++ palindromo.py
@@ -7,12 +7,12 @@ def verificar_palindromo(texto):
     word = word.translate(str.maketrans('', '', string.punctuation))
     word = word.lower()
 
-    print(word)
-
+    #print(word)
     if word == word[::-1]:
         return f"{texto} é palindromo"
     else:
         return f"{texto} não é palindromo"
+
 
 teste1 = verificar_palindromo("Anotaram a data da maratona")
 print(teste1)
